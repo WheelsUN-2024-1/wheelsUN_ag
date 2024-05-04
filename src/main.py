534790458ddc2c4affe88a60ebe4ae9e866f95c8
Carlos Cortes <ccortesra@unal.edu.co>
2024-05-04 15:52:39 -0500

    Origins

--- src/main.py
+++ src/main.py
@@ -21,13 +21,10 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-origins = [
-    "http://localhost:9200",
-]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
